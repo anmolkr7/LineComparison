a87fa7ecc32f49a8053993223c2e05ca47957134
anmolkr7 <anmolojha952@gmail.com>
2025-12-10 16:02:35 +0530

    UC2:Compare lengths using equals method

--- src/Main.java
+++ src/Main.java
@@ -5,23 +5,47 @@ public class Main{
 
         Scanner sc=new Scanner(System.in);
 
-        System.out.println("Enter x1, y1:");
+        //Input for Line1
+        System.out.println("Enter x1, y1 for Line 1:");
         int x1=sc.nextInt();
         int y1=sc.nextInt();
 
-        System.out.println("Enter x2, y2:");
+        System.out.println("Enter x2, y2 for Line 1:");
         int x2=sc.nextInt();
         int y2=sc.nextInt();
 
-        //Creating Point objects
+        //Creating Point and Line Objects for Line 1
         Point p1=new Point(x1,y1);
         Point p2=new Point(x2,y2);
+        Line line1=new Line(p1,p2);
 
-        //Creating Line object
-        Line line=new Line(p1,p2);
+        //Input for Line 2
+        System.out.println("Enter x1, y1 for Line 2:");
+        int a1=sc.nextInt();
+        int b1=sc.nextInt();
 
-        //Calculating and displaying length
-        double length=line.getLength();
-        System.out.println("Length of the Line is: "+length);
+        System.out.println("Enter x2, y2 for Line 2:");
+        int a2=sc.nextInt();
+        int b2=sc.nextInt();
+
+        //Creating Point and Line Objects for Line 2
+        Point q1=new Point(a1,b1);
+        Point q2=new Point(a2,b2);
+        Line line2=new Line(q1,q2);
+
+
+        //Calculating and displaying length of both lines
+        double length1=line1.getLength();
+        System.out.println("Length of the Line 1 is: "+length1);
+
+        double length2=line2.getLength();
+        System.out.println("Length of the Line 1 is: "+length2);
+
+        // Using equals() to check equality of two lines
+        if(line1.equals(line2)){
+            System.out.println("Both lines are EQUAL.");
+        } else {
+            System.out.println("Both lines are NOT EQUAL.");
+        }
     }
 }
