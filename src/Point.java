c7ff954f031149ba7f8cfc44942a721a25b1adf7
anmolkr7 <anmolojha952@gmail.com>
2025-12-10 15:48:41 +0530

    UC1:Point class to store x and y co-ordinates

---
+++ src/Point.java
@@ -0,0 +1,10 @@
+public class Point {
+    int x;
+    int y;
+
+    // Constructor to initialize point co-ordinates
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
